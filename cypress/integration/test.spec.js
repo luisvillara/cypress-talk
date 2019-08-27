5c7041e1112aa0e6e76bf6b02ecc92b6de47c68e
Luis Villalobos <alfredovillalobo@gmail.com>
2019-08-26 21:45:41 -0500

    Write some tests

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,46 @@
+context('Testing test', () => {
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000/');
+  // });
+
+  before(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Validar si la tarea por defecto es creada', () => {
+    cy.get('.todo-list li').should('have.length', 1);
+    cy.get('.todo-list li:first .view label').contains('Use Redux');
+  });
+
+  it('Agregar 1 todo', () => {
+    cy.get('.new-todo')
+      .type('Learn testing using Cypress{enter}');
+    cy.get('.todo-list li').should('have.length', 2);
+  });
+
+  it('Editar la primera todo agregada', () => {
+    cy.get('.todo-list li:first .view label').dblclick();
+    cy.get('.todo-list').within(() => {
+      // cy.get('.editing .edit')
+      //   .type('{selectall}{del}Use Cypress{enter}');
+      cy.get('.editing .edit')
+        .clear()
+        .type('Use Cypress{enter}');
+    });
+  });
+
+  it('Marcar la primera tarea como completada', () => {
+    cy.get('.todo-list li:first').within(() => {
+      cy.get('.toggle').click();
+    });
+    cy.get('.todo-list li:first').should('have.class', 'completed');
+  });
+
+  it('Eliminar la ultima tarea agregada', () => {
+    cy.get('.todo-list li:last')
+      .within(() => {
+        cy.get('.destroy').invoke('show').click();
+      });
+    cy.get('.todo-list li').should('have.length', 1);
+  });
+});
